0dfeb7f03a3097934792ad046f8af46ca683ccb1
wided-19 <widedchedly1@gmail.com>
2021-07-22 12:23:35 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,117 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Button, Form, Row, Col,  } from 'react-bootstrap';
+
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <h1>Welcome</h1>
+
+      <header className="App-header"> 
+      
+      <Form>
+        <table class="elements"  > 
+      <Row>
+      
+          <Col md>
+            <tr>
+            <Form.Label>First name</Form.Label>
+            </tr>
+            <td>
+            <Form.Control placeholder="First name" />
+            </td>
+            
+          </Col>
+          <Col md>
+            <tr>
+            <Form.Label>Last name</Form.Label>
+            </tr>
+            <td>
+            <Form.Control placeholder="Last name" />
+            </td>
+          </Col>
+        </Row>
+
+        <Form.Group className="mb-3" controlId="formGridAddress2">
+          <tr>
+    <Form.Label>Address </Form.Label>
+    </tr>
+    <td>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+    </td>
+  </Form.Group>
+
+
+  <Form.Group className="mb-3" controlId="formBasicCountry">
+    <tr>
+    <Form.Label>Country</Form.Label>
+    </tr>
+    <td>
+    <Form.Control type="Country" placeholder="Country" />
+    </td>
+
+  </Form.Group>
+
+  <div class="style-select">
+          <select data-id="domain-switcher">
+            <option>Choose City</option>
+            <option value="http://www.doctoralia.com.tu">tunis</option>
+
+            <option value="http://www.doctoraliar.so">Sousse</option>
+            <option value="http://www.doctoraliar.sf">Sfax</option>
+            <option value="http://www.doctoraliar.na">Nabeul</option>
+            </select>
+            </div>
+                             
+
+
+
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <tr>
+    <Form.Label>Email address</Form.Label>
+    </tr>
+    <td>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted"> 
+
+    </Form.Text>
+    </td>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <tr>
+    <Form.Label>Password</Form.Label>
+    </tr>
+    <td>
+    <Form.Control type="password" placeholder="Password" />
+    </td>
+  </Form.Group>
+
+
+  
+  <Form.Group className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+   
+  </Form.Group>
+  <tr>
+    <td>
+  
+   
+  <Button variant="primary" type="submit">
+    Submit
+
+  </Button></td></tr>
+  </table>
+  
+
+</Form>
       </header>
     </div>
   );
 }
+
 
 export default App;
